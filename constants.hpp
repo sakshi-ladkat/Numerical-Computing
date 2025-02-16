3efc5ce5f6471124210c0e117eb5059827da0ddc
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2025-02-16 23:36:08 +0530

    constants header file

---
+++ constants.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+class constants
+{
+public:
+   static constexpr double TOL_3 = 0.001;
+   static constexpr double TOL_4 = 0.0001;
+   static constexpr double TOL_5 = 0.00001;
+   static constexpr double TOL_6 = 0.000001;
+   static constexpr double min = -100;
+   static constexpr double max = 100;
+};
