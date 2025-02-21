bc86e09bd9d15c0d008d9758f3c6e6526bc04d2f
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2025-02-21 12:57:27 +0530

    Matrix Header

---
+++ Matrix.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+class Matrix
+{
+
+private:
+    int row;
+    int col;
+    vector<vector<int>> values;
+
+public:
+    Matrix(int r, int c)
+    {
+        row = r;
+        col = c;
+        values.resize(r, vector<int>(c));
+    }
+    static Matrix Input(const string &filepath);
+    void Display() const;
+    bool isSymmetric() const;
+    bool isIdentity() const;
+    Matrix Addition(const Matrix &second) const;
+    Matrix Subtraction(const Matrix &second) const;
+
+    //~Matrix();
+};
