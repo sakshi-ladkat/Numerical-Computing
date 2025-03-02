6c14d582adf538991c87fb94792b0afea06df125
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2025-03-02 21:02:34 +0530

    Matrix Header File

--- Matrix/Matrix.hpp
+++ Matrix/Matrix.hpp
@@ -16,7 +16,7 @@ public:
     {
         row = r;
         col = c;
-        values.vector<(r, vector<int>(c))>;
+        values = vector<vector<int>>(r, vector<int>(c, 0));
     }
     static Matrix Input(const string &filepath);
     void Display() const;
@@ -24,6 +24,9 @@ public:
     bool isIdentity() const;
     Matrix Addition(const Matrix &second) const;
     Matrix Subtraction(const Matrix &second) const;
+    Matrix LowerTriangular();
+    Matrix UpperTriangular() const;
+    Matrix GaussianElimination() const;
 
     //~Matrix();
 };
